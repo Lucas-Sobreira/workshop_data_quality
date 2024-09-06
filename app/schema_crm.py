451d657e99fea95dc2d195e510908fb9c3691f8e
Lucas-Sobreira <lucas-andrade2011@live.com>
2024-09-06 15:53:25 -0300

    final do projeto

--- app/schema_crm.py
+++ app/schema_crm.py
@@ -1,5 +1,5 @@
 import pandera as pa
-from pandera.typing import Series
+from pandera.typing import DataFrame, Series
 
 email_regex = r"[^@]+@[^@]+\.[^@]+"
 
@@ -18,13 +18,19 @@ class ProdutoSchemaEmail(pa.DataFrameModel):
         categoria (Series[str]): Categoria do produto.
         email (Series[str]): E-mail associado ao produto, deve seguir o formato padrão de e-mails.
     """
-    id_produto: Series[int] = pa.Field(ge=1, le=100)
+    id_produto: Series[int]
     nome: Series[str]
     quantidade: Series[int] = pa.Field(ge=20, le=200)
-    preco: Series[float] = pa.Field(ge=5.0, le=120.0)
+    preco: Series[float] = pa.Field(ge=05.0, le=120.0)
     categoria: Series[str]
     email: Series[str] = pa.Field(regex=email_regex)
 
     class Config:
-        coerce = True # Força conversão de tipos
-        strict = True # Permite colunas extras
+        coerce = True
+        strict = True
+
+class ProdutoSchemaEmailKPI(ProdutoSchemaEmail):
+
+    valor_total_estoque: Series[float] = pa.Field(ge=0)  # O valor total em estoque deve ser >= 0
+    categoria_normalizada: Series[str]  # Assume-se que a categoria será uma string, não precisa de check específico além de ser uma string
+    disponibilidade: Series[bool]  # Disponibilidade é um booleano, então não precisa de check específico
