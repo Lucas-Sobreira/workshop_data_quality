5a6af99e322d87dd1a71c1afddbf5e9ea39f585a
Lucas-Sobreira <lucas-andrade2011@live.com>
2024-09-06 11:09:27 -0300

    update: Comentario da funcao

--- app/schema_crm.py
+++ app/schema_crm.py
@@ -5,7 +5,18 @@ email_regex = r"[^@]+@[^@]+\.[^@]+"
 
 class ProdutoSchemaEmail(pa.DataFrameModel):
     """
-    Define o Schema do Database
+    Define o esquema para a validação de dados de produtos com Pandera.
+    
+    Este esquema inclui campos básicos para produtos, incluindo um campo de e-mail
+    validado por uma expressão regular.
+
+    Attributes:
+        id_produto (Series[int]): Identificador do produto, deve estar entre 1 e 20.
+        nome (Series[str]): Nome do produto.
+        quantidade (Series[int]): Quantidade disponível do produto, deve estar entre 20 e 200.
+        preco (Series[float]): Preço do produto, deve estar entre 5.0 e 120.0.
+        categoria (Series[str]): Categoria do produto.
+        email (Series[str]): E-mail associado ao produto, deve seguir o formato padrão de e-mails.
     """
     id_produto: Series[int] = pa.Field(ge=1, le=100)
     nome: Series[str]
